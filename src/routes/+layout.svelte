f7c7bc45300969fbfbc52c3661dbf9ce0985065c
tt954 <61483705+tt954@users.noreply.github.com>
2025-01-24 15:40:06 -0500

    refactor: remove unused css

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,34 +6,7 @@
 	<main>
 		<slot />
 	</main>
-
-	<footer>made by Tiffany.</footer>
 </div>
 
 <style>
-	.app {
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
-		background: linear-gradient(to bottom right, #f3f4f6, #eaeff5); /* Soft gradient */
-	}
-
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
-	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,3 @@
-<script lang="ts">
-	import { page } from "$app/stores";
-
-	let isExpanded: boolean = false;
-	let isCopied: boolean = false;
-	const email: string = "tiffany11459@gmail.com";
-
-	function toggleContacts() {
-		isExpanded = !isExpanded;
-	}
-
-	async function copyEmail() {
-		try {
-			await navigator.clipboard.writeText(email);
-			isCopied = true;
-			setTimeout(() => (isCopied = false), 2000); // reset isCopied after 2 secs
-		} catch (err) {
-			console.error("Failed to copy: ", err);
-		}
-	}
-</script>
-
 <svelte:head>
 	<title>tieulam thai</title>
 	<meta name="tieulam thai" content="personal website" />
@@ -31,27 +9,13 @@
 		<h2 class="hero-name">( tiffany thai )</h2>
 	</div>
 
-	<nav class="menu">
-		<ul>
-			<li aria-current={$page.url.pathname === "/about" ? "page" : undefined}>
-				<a href="/about">About Me</a>
-			</li>
-			<li aria-current={$page.url.pathname === "/experience" ? "page" : undefined}>
-				<a href="/experience">Experience</a>
-			</li>
-			<li aria-current={$page.url.pathname === "/fun" ? "page" : undefined}>
-				<a href="/fun">Fun</a>
-			</li>
-		</ul>
-	</nav>
-
 	<div class="footer">
 		<div class="footer-contact">
 			<p>+1 817 881 1459</p>
 			<p>tiffany11459@gmail.com preferred</p>
 		</div>
 		<div class="footer-extra">
-			<p>2025 portfolio</p>
+			<a class="page-link" href="/experience">2025 portfolio</a>
 			<p>(✿)</p>
 		</div>
 	</div>
@@ -59,15 +23,16 @@
 
 <style>
 	section.hero {
+		align-items: center;
+		color: var(--color-blue);
 		display: flex;
 		flex-direction: column;
-		align-items: center;
-		justify-content: space-between;
 		height: 100vh;
-		width: max-content;
-		text-align: center;
-		padding: 7.25rem 1rem;
+		justify-content: space-between;
 		margin: 0 auto;
+		padding: 7.25rem 1rem;
+		text-align: center;
+		width: max-content;
 	}
 
 	.hero-title {
@@ -85,11 +50,11 @@
 	}
 
 	.footer {
-		display: flex;
-		justify-content: space-between;
 		align-items: flex-end;
+		display: flex;
 		font-family: var(--font-oswald);
 		font-size: var(--font-size-h4);
+		justify-content: space-between;
 		text-transform: uppercase;
 		width: 100%;
 	}
@@ -99,5 +64,12 @@
 	.footer-extra {
 		display: flex;
 		gap: 1rem;
+	}
+
+	.page-link {
+		text-decoration: none;
+	}
+	.page-link:visited {
+		color: inherit;
 	}
 </style>
