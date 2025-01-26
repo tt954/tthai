e46f21a3fb6aa825b20b8bbe1cdc37ea48a8efcc
tt954 <61483705+tt954@users.noreply.github.com>
2025-01-26 17:35:46 -0500

    refactor: turn folder into a component

---
+++ src/components/Folder.svelte
@@ -0,0 +1,55 @@
+<script>
+	export let href;
+	export let label;
+</script>
+
+<a class="folder" {href}>
+	<div class="folder-icon">
+		<i class="fa-solid fa-folder folder-open"></i>
+		<i class="fa-regular fa-folder-open folder-close"></i>
+	</div>
+	<span class="folder-label">{label}</span>
+</a>
+
+<style>
+	.folder {
+		display: flex;
+		text-decoration: none;
+		flex-direction: column;
+		align-items: center;
+		justify-content: space-between;
+		height: max-content;
+	}
+	.folder-icon {
+		position: relative;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+	.folder-icon .folder-open,
+	.folder-icon .folder-close {
+		font-size: 7.5rem;
+		color: var(--color-blue);
+		position: absolute;
+		transition: opacity 0.3s ease;
+	}
+	.folder-icon .folder-close {
+		opacity: 0;
+	}
+	.folder-icon:hover .folder-open {
+		opacity: 0;
+	}
+	.folder-icon:hover .folder-close {
+		opacity: 1;
+	}
+	.folder-label {
+		display: block;
+		/* font-weight: 500; */
+		font-family: var(--font-oswald);
+		margin: 3.5rem;
+		font-size: 1.5rem;
+		color: var(--color-blue);
+		text-transform: uppercase;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,7 @@
+<script>
+	import Folder from "$components/Folder.svelte";
+</script>
+
 <svelte:head>
 	<title>tieulam thai</title>
 	<meta name="tieulam thai" content="personal website" />
@@ -10,15 +14,8 @@
 	</div>
 
 	<ul class="experience">
-		<li>
-			<a class="folder" href="/experience">
-				<div class="folder-icon">
-					<i class="fa-solid fa-folder folder-open"></i>
-					<i class="fa-regular fa-folder-open folder-close"></i>
-				</div>
-				<span class="folder-label">work</span>
-			</a>
-		</li>
+		<li><Folder href="/experience" label="work" /></li>
+		<li><Folder href="/fun" label="fun" /></li>
 	</ul>
 
 	<div class="footer">
@@ -65,46 +62,6 @@
 		display: flex;
 		list-style: none;
 		gap: 1rem;
-	}
-	.folder {
-		display: flex;
-		text-decoration: none;
-		flex-direction: column;
-		align-items: center;
-		justify-content: space-between;
-		height: max-content;
-	}
-	.folder-icon {
-		position: relative;
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
-	.folder-icon .folder-open,
-	.folder-icon .folder-close {
-		font-size: 7.5rem;
-		color: var(--color-blue);
-		position: absolute;
-		transition: opacity 0.3s ease;
-	}
-	.folder-icon .folder-close {
-		opacity: 0;
-	}
-	.folder-icon:hover .folder-open {
-		opacity: 0;
-	}
-	.folder-icon:hover .folder-close {
-		opacity: 1;
-	}
-	.folder-label {
-		display: block;
-		/* font-weight: 500; */
-		font-family: var(--font-oswald);
-		margin: 3.5rem;
-		font-size: 1.5rem;
-		color: var(--color-blue);
-		text-transform: uppercase;
 	}
 
 	.footer {
